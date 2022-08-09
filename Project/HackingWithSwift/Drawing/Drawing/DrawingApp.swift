a9d2e92158e373fd013333fc548c259893012606
마석우 <masagu96@naver.com>
2022-08-09 22:08:23 +0900

    add Drawing

---
+++ Project/HackingWithSwift/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by 마석우 on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
