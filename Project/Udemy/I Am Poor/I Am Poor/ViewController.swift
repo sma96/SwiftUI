6e703c2f3cd4c354d15fe5499439a2c5c01be9a2
마석우 <masagu96@naver.com>
2022-08-10 14:44:37 +0900

    add Udemy

---
+++ Project/Udemy/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 마석우 on 2022/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
